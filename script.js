e2626f109053d33893d95702e712a91e053ccbbe
Cristian <vamix2012@gmail.com>
2022-01-14 17:02:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,197 @@
+let dimi = [
+    "\nGeneza 1,2\n Pag. 1",
+    "\nGeneza 3,4,5\n Pag. 3",
+    "\nGeneza 6,7,8\n Pag. 6",
+    "\nGeneza 9,10,11\n Pag. 9",
+    "\nGeneza 12,13,14\n Pag. 12",
+    "\nGeneza 15,16,17\n Pag. 14",
+    "\nGeneza 18,19 \nPag. 17",
+    "\nGeneza 20,21,22\n Pag. 20",
+    "\nGeneza 23,24\n Pag. 23",
+    "\nGeneza 25,26\n Pag. 26",
+    "\nGeneza 27,28\n Pag. 29",
+    "\nGeneza 29,30\n Pag. 31",
+    "\nGeneza 31,32\n Pag. 34",
+    "\nGeneza 33,34,35\n Pag. 37",
+    "\nGeneza 36,37\n Pag. 40",
+    "\nGeneza 38,39,40\n Pag. 43",
+    "\nGeneza 41\n Pag. 45",
+    "\nGeneza 42,43\n Pag. 48",
+    "\nGeneza 44,45\n Pag. 50",
+    "\nGeneza 46,47,48\n Pag. 53",
+    "\nGeneza 49,50\n Pag. 56",
+    "\nExod 1,2,3\n Pag. 59",
+    "\nExod 4,5,6\n Pag. 62",
+    "\nExod 7,8\n Pag. 65",
+    "\nExod 9,10\n Pag. 67",
+    "\nExod 11,12\n Pag. 70",
+    "\nExod 13,14,15\n Pag. 72",
+    "\nExod 16,17,18\n Pag. 76",
+    "\nExod 19,20,21\n Pag. 79",
+    "\nExod 22,23,24\n Pag. 83",
+    "\nExod 25,26\n Pag. 86",
+    "\nExod 27,28\n Pag. 89",
+    "\nExod 29,30\n Pag. 91",
+    "\nExod 31,32,33\n Pag. 95",
+    "\nExod 34,35,36\n Pag. 98",
+    "\nExod 37,38\n Pag. 102",
+    "\nExod 39,40\n Pag. 104",
+    "\n\n Pag. ",
+
+
+];
+
+let seara = [
+    "\nMatei 1\n Pag. 923",
+    "\nMatei 2\n Pag. 924",
+    "\nMatei 3\n Pag. 925",
+    "\nMatei 4\n Pag. 925",
+    "\nMatei 5:1-26\n Pag. 926",
+    "\nMatei 5:27-48\n Pag. 927",
+    "\nMatei 6 \nPag. 928",
+    "\nMatei 7\n Pag. 929",
+    "\nMatei 8\n Pag. 930",
+    "\nMatei 9:1-17\n Pag. 931",
+    "\nMatei 9:18-38\n Pag. 932",
+    "\nMatei 10:1-23\n Pag. 933",
+    "\nMatei 10:24-42\n Pag. 934",
+    "\nMatei 11\n Pag. 934",
+    "\nMatei 12:1-21\n Pag. 936",
+    "\nMatei 12:22-50\n Pag. 936",
+    "\nMatei 13:1-32\n Pag. 937",
+    "\nMatei 13:33-58\n Pag. 939",
+    "\nMatei 14:1-21\n Pag. 940",
+    "\nMatei 14:22-36\n Pag. 940",
+    "\nMatei 15\n Pag. 941",
+    "\nMatei 16\n Pag. 942",
+    "\nMatei 17\n Pag. 943",
+    "\nMatei 18:1-20\n Pag. 944",
+    "\nMatei 18:21-35\n Pag. 945",
+    "\nMatei 19:1-15\n Pag. 945",
+    "\nMatei 19:16-30\n Pag. 946",
+    "\nMatei 20:1-16\n Pag. 947",
+    "\nMatei 20:17-34\n Pag. 947",
+    "\nMatei 21:1-22\n Pag. 948",
+    "\nMatei 21:23-46\n Pag. 948",
+    "\nMatei 22:1-22\n Pag. 949",
+    "\nMatei 22:23-46\n Pag. 950",
+    "\nMatei 23:1-22\n Pag. 951",
+    "\nMatei 23:23-39\n Pag. 951",
+    "\nMatei 24:1-22\n Pag. 952",
+    "\nMatei 24:23-51\n Pag. 953",
+    "\nMatei 25:1-30\n Pag. 954",
+    "\nMatei 25:31-46\n Pag. 955",
+    "\nMatei 26:1-19\n Pag. 955",
+    "\nMatei 26:20-54\n Pag. 956",
+    "\nMatei 26:55-75\n Pag. 957",
+    "\nMatei 27:1-31\n Pag. 958",
+    "\nMatei 27:32-66\n Pag. 959",
+    "\nMatei 28\n Pag. 960",
+    "\nMarcu 1:1-22\n Pag. 961",
+    "\nMarcu 1:23-45\n Pag. 962",
+    "\nMarcu 2\n Pag. ",
+    "\nMarcu 3:1-21\n Pag. ",
+    "\nMarcu 3:22-35\n Pag. ",
+    "\nMarcu 4:1-20\n Pag. ",
+    "\nMarcu 4:21-41\n Pag. ",
+    "\nMarcu 5:1-20\n Pag. ",
+    "\nMarcu 5:21-43\n Pag. ",
+    "\nMarcu 6:1-32\n Pag. ",
+    "\nMarcu 6:33-56\n Pag. ",
+    "\nMarcu 7:1-13\n Pag. ",
+    "\nMarcu 7:14-37\n Pag. ",
+    "\nMarcu 8:1-21\n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+    "\nMarcu \n Pag. ",
+
+];
+
+
+
+
+
+var now = new Date();
+var start = new Date(now.getFullYear(), 0, 0);
+var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+var oneDay = 1000 * 60 * 60 * 24; var day = Math.floor(diff / oneDay);
+
+
+let date = new Date();
+
+let d = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+let mon = date.getMonth() + 1;
+let m = mon < 10 ? "0" + mon : mon;
+let y = date.getFullYear();
+let msg = "";
+
+
+
+
+function init() {
+    document.getElementById("data").innerHTML = d + "." + m + "." + y;
+
+    document.getElementById("Dimineata").innerHTML = dimi[day - 1];
+
+    document.getElementById("Seara").innerHTML = seara[day - 1];
+
+
+
+    setMsg();
+    calc();
+
+}
+
+function setMsg() {
+    msg = "_*Pasajul Zilei*_\n\n*Data:*  ```" + d + "." + m + "." + y + "```\n\n*Dimineata:*  ```" + dimi[day - 1] + "```\n\n*Seara:* ```" + seara[day - 1] + "```" + "\n\n_Sursa: hrana-zilnica.info_";
+}
+
+function share() {
+    if (navigator.share) {
+        navigator.share({
+            title: `Mesaj`,
+            text: `${msg}`
+        })
+    }
+}
+
+function shareVerse() {
+    verse = "```" + document.getElementById("verse").innerText + "```\n\n_Sursa: hrana-zilnica.info_"
+    if (navigator.share) {
+        navigator.share({
+            title: `Mesaj`,
+            text: `${verse}`
+        })
+    }
+}
+
+function shareGand() {
+    gand = "```" + document.getElementById("gand").innerText + "```\n\n_Sursa: hrana-zilnica.info_"
+    if (navigator.share) {
+        navigator.share({
+            title: `Mesaj`,
+            text: `${gand}`
+        })
+    }
+}
+
+function calc() {
+    let total = dimi.length + seara.length;
+    let progress = (total * 100) / 730;
+    console.log("dimi ", dimi.length + " din 365 ", "\n sera ", seara.length + " din 365");
+    document.getElementById("progress").innerHTML = "Baza de date 'Pasajul zilei' este " + progress.toFixed(2) + "% completa, doresti sa te implici la completarea bazei de date?     <a href='./implicare.html'>Click aici</a><br> "
+}
