a616e58b60ee181b67bf5e47d16a92c3be208a7f
Cristian <vamix2012@gmail.com>
2022-02-21 23:10:49 +0100

    added new features

--- canvas.js
+++ canvas.js
@@ -1,32 +1,32 @@
-let canvas;
-let ctx;
+let canvas1;
+let ctx1;
 let color = "black";
 let size = 32;
 
 function initCanvas() {
-    canvas = document.getElementById("canvas");
-    ctx = canvas.getContext("2d");
+    canvas1 = document.getElementById("canvas1");
+    ctx1 = canvas1.getContext("2d");
 
-    canvas.width = 350;
-    canvas.height = 250;
+    canvas1.width = 350;
+    canvas1.height = 250;
 
     let img = new Image();
     img.onload = function () {
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        ctx.globalAlpha = 1;
-        ctx.fillStyle = color;
-        ctx.font = `${size}px Comic Sans MS`;
-        ctx.textAlign = "center";
-        ctx.fillText("Pasajul Zilei", canvas.width / 2, size);
-        ctx.font = `${size - 10}px Comic Sans MS`;
-        ctx.fillText(d + "." + m + "." + y, canvas.width / 2, size * 2);
-        ctx.fillText("Dimineata", canvas.width / 2, size * 4);
-        ctx.fillText(dimi[day - 1], canvas.width / 2, size * 5);
-        ctx.fillText("Seara", canvas.width / 2, size * 6);
-        ctx.fillText(seara[day - 1], canvas.width / 2, size * 7);
-        ctx.textAlign = "end";
-        ctx.font = `${size - 20}px Comic Sans MS`;
-        ctx.fillText("hrana-zilnica.info", canvas.width - 10, size * 7.6);
+        ctx1.drawImage(img, 0, 0, canvas1.width, canvas1.height);
+        ctx1.globalAlpha = 1;
+        ctx1.fillStyle = color;
+        ctx1.font = `${size}px Comic Sans MS`;
+        ctx1.textAlign = "center";
+        ctx1.fillText("Pasajul Zilei", canvas1.width / 2, size);
+        ctx1.font = `${size - 10}px Comic Sans MS`;
+        ctx1.fillText(d + "." + m + "." + y, canvas1.width / 2, size * 2);
+        ctx1.fillText("Dimineata", canvas1.width / 2, size * 4);
+        ctx1.fillText(dimi[day - 1], canvas1.width / 2, size * 5);
+        ctx1.fillText("Seara", canvas1.width / 2, size * 6);
+        ctx1.fillText(seara[day - 1], canvas1.width / 2, size * 7);
+        ctx1.textAlign = "end";
+        ctx1.font = `${size - 20}px Comic Sans MS`;
+        ctx1.fillText("hrana-zilnica.info", canvas1.width - 10, size * 7.6);
 
     };
     img.src = '2.jpeg';
@@ -37,17 +37,11 @@ function initCanvas() {
 
 
 function download() {
-    const msg = canvas.toDataURL();
-    if (navigator.share) {
-        navigator.share({
-            title: `Mesaj`,
-            text: `${msg}`
-        })
-    }
-    // const link = document.createElement("a");
-    // link.href = image;
-    // link.download = `day${day - 1}.png`;
-    // link.click();
+    const image = canvas1.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = `day${day - 1}.png`;
+    link.click();
 
 }
 
--- engine.js
+++ engine.js
@@ -11,30 +11,30 @@ let d = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
 let mon = date.getMonth() + 1;
 let m = mon < 10 ? "0" + mon : mon;
 let y = date.getFullYear();
-let msg = "";
 
 
 
 
-function init() {
-    document.getElementById("data").innerHTML = d + "." + m + "." + y;
-    document.getElementById("Dimineata").innerHTML = dimi[day - 1];
-    document.getElementById("Seara").innerHTML = seara[day - 1];
-    setMsg();
-}
 
-function setMsg() {
-    msg = "_*Pasajul Zilei*_\n\n*Data:*  ```" + d + "." + m + "." + y + "```\n\n*Dimineata:*  ```" + dimi[day - 1] + "```\n\n*Seara:* ```" + seara[day - 1] + "```" + "\n\n_Sursa: hrana-zilnica.info_";
-}
+// function init() {
+//     document.getElementById("data").innerHTML = d + "." + m + "." + y;
+//     document.getElementById("Dimineata").innerHTML = dimi[day - 1];
+//     document.getElementById("Seara").innerHTML = seara[day - 1];
+//     setMsg();
+// }
 
-function share() {
-    if (navigator.share) {
-        navigator.share({
-            title: `Mesaj`,
-            text: `${msg}`
-        })
-    }
-}
+// function setMsg() {
+//     msg = "_*Pasajul Zilei*_\n\n*Data:*  ```" + d + "." + m + "." + y + "```\n\n*Dimineata:*  ```" + dimi[day - 1] + "```\n\n*Seara:* ```" + seara[day - 1] + "```" + "\n\n_Sursa: hrana-zilnica.info_";
+// }
+
+// function share() {
+//     if (navigator.share) {
+//         navigator.share({
+//             title: `Mesaj`,
+//             text: `${msg}`
+//         })
+//     }
+// }
 
 function shareVerse() {
     verse = "```" + document.getElementById("verse").innerText + "```\n\n_Sursa: hrana-zilnica.info_"
